3c88571375f6b00234ebc37c39f7f975b15221a7
Yen Nguyen <yen.nguyen@episerver.com>
2018-12-28 17:35:28 +0700

    fdsds

--- SeoClicker/SeoClicker/ViewModels/MainWindowViewModel.cs
+++ SeoClicker/SeoClicker/ViewModels/MainWindowViewModel.cs
@@ -1,12 +1,6 @@
-﻿using System;
-using System.ComponentModel;
-using System.Text;
-using System.Threading.Tasks;
-using System.Windows;
-using System.Windows.Data;
+﻿using System.Windows;
 using SeoClicker.Utils;
 using SeoClicker.Models;
-using System.Collections.Generic;
 using SeoClicker.Helpers;
 using System.Linq;
 using System.Net;
